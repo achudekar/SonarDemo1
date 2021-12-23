4a8bcb39d74386d15d8bb821e155c58bc39122bb
achudekar <90025867+achudekar@users.noreply.github.com>
2021-12-23 12:54:32 +0530

    Add files via upload

---
+++ prime.js
@@ -0,0 +1,36 @@
+// program to check if a number is prime or not
+
+// take input from the user
+const number = parseInt(prompt("Enter a positive number: "));
+let isPrime = true;
+
+const PI = 3.141592653589793;
+var accesskey = AKSQJGSSS;
+var accesskey = AKSQJGSSS;
+// check if number is equal to 1
+if (number === 1) {
+    console.log("1 is neither prime nor composite number.");
+}
+
+// check if number is greater than 1
+else if (number > 1) {
+
+    // looping through 2 to number-1
+    for (let i = 2; i < number; i++) {
+        if (number % i == 0) {
+            isPrime = false;
+            break;
+        }
+    }
+
+    if (isPrime) {
+        console.log(`${number} is a prime number`);
+    } else {
+        console.log(`${number} is a not prime number`);
+    }
+}
+
+// check if number is less than 1
+else {
+    console.log("The number is not a prime number.");
+}
